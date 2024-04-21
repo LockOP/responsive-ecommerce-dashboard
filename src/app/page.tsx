5f2ae663e4cc6e09c45648ca9a78fc07b732e57c
LockOP <madhavaarul@gmail.com>
2024-04-21 13:16:13 +0530

    ver 8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,25 @@ import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
 import Landingpage from "./LandingPage/page";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/Inventory/GridView");
+  }, []);
+
   return (
     <div className="flex bg-slate-50 flex-col">
       {/* <Navbar/>
     <Landingpage/>
     <Footer/> */}
-      <Link href={"Inventory/GridView"} className="text-blue-700 underline self-center">
+      <Link
+        href={"Inventory/GridView"}
+        className="text-blue-700 underline self-center"
+      >
         Continue to Inventory/GridView Page
       </Link>
     </div>
